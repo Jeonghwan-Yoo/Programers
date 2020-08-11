1680ae78170f42e9c2fa91139d116158bc240669
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-08-11 14:13:53 +0900

    Bracket conversion

---
+++ KakaoBlind2020/B.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool CheckU(string const &p, string &u, string &v)
+{
+    bool check = true;
+    int cnt = 0;
+    for (int i = 0; i < (int)p.size(); ++i)
+    {
+        if (p[i] == '(')
+            ++cnt;
+        else
+            --cnt;
+        if (cnt < 0)
+            check = false;
+        u += p[i];
+        if (cnt == 0)
+            break;
+    }
+
+    for (int i = (int)u.size(); i < (int)p.size(); ++i)
+        v += p[i];
+
+    return check;
+}
+
+string solution(string p) {
+    string answer;
+    if (p.empty())
+        return "";
+
+    string u, v;
+    bool check = CheckU(p, u, v);
+    if (check == true)
+    {
+        u += solution(v);
+        answer += u;
+    }
+    else
+    {
+        string s;
+        s += "(";
+        s += solution(v);
+        s += ")";
+        for (int i = 1; i < (int)u.size() - 1; ++i)
+            s += !(u[i] - '(') + '(';
+        answer += s;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string in;
+    getline(cin, in);
+    string ret = solution(in);
+    cout << ret;
+
+    return 0;
+}
