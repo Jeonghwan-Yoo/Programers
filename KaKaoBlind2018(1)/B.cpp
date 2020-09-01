a6d41652509ba77d3b30b78432e4c61ad8d82d93
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-09-02 04:14:08 +0900

    Dart game

--- KaKaoBlind2018(1)/B.cpp
+++ KaKaoBlind2018(1)/B.cpp
@@ -37,3 +37,39 @@ int solution(string dartResult) {
     
     return answer;
 }
+
+// #include <string>
+// #include <regex>
+// #include <iostream>
+
+// using namespace std;
+
+// int solution(string dartResult) {
+//     int answer = 0;
+    
+//     int game[3] = { 0, };
+//     regex re("([0-9]+)(S|D|T)(\\*|#*)");
+//     int pos = 0;
+//     for (auto i = sregex_iterator(dartResult.begin(), dartResult.end(), re); i != sregex_iterator(); ++i)
+//     {
+//         game[pos] = stoi(i->str(1));
+//         if (i->str(2) == "D")
+//             game[pos] *= game[pos];
+//         else if (i->str(2) == "T")
+//             game[pos] *= game[pos] * game[pos];
+        
+//         if (i->str(3) == "*")
+//         {
+//             game[pos] <<= 1;
+//             if (pos - 1 >= 0)
+//                 game[pos - 1] <<= 1;
+//         }
+//         else if (i->str(3) == "#")
+//             game[pos] *= -1;
+        
+//         ++pos;
+//     }
+//     answer += game[0] + game[1] + game[2];
+    
+//     return answer;
+// }
