86aa140d086b25370ae522eaf3ffca7ec37e3a3d
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-09-22 22:59:58 +0900

    Matching score

--- KaKaoBlind2019/F.cpp
+++ KaKaoBlind2019/F.cpp
@@ -69,3 +69,70 @@ int solution(string word, vector<string> pages) {
     
     return answer;
 }
+
+// #include <string>
+// #include <vector>
+// #include <regex>
+// #include <unordered_map>
+// #include <algorithm>
+// #include <iostream>
+
+// using namespace std;
+
+// int solution(string word, vector<string> pages) {
+//     int answer = 0;
+    
+//     int n = (int)pages.size();
+//     vector<int> basic(n);
+//     vector<int> external(n);
+//     vector<double> link(n);
+//     vector<double> matching(n);
+    
+//     unordered_map<string, int> um;
+    
+//     regex reWord(word + "[^a-z]", regex::icase);
+//     regex reUrl("content=\"https://(.+?)\"\/>");
+//     regex reLink("<a href=\"https://(.+?)\">");
+    
+//     for (int i = 0; i < n; ++i)
+//     {
+//         int l = pages[i].find("<head>");
+//         int r = pages[i].find("</head>");
+//         for (auto j = sregex_iterator(pages[i].begin() + l, pages[i].begin() + r, reUrl); j != sregex_iterator(); ++j)
+//             um[j->str(1)] = i;
+        
+//         l = pages[i].find("<body>");
+//         r = pages[i].find("</body>");
+//         for (auto j = sregex_iterator(pages[i].begin() + l, pages[i].begin() + r, reWord); j != sregex_iterator(); ++j)
+//         {
+//             if ((int)j->str().size() > (int)word.size() && isalpha(pages[i][j->position() + l - 1]))
+//                 continue;
+//             ++basic[i];
+//         }
+        
+//         for (auto j = sregex_iterator(pages[i].begin() + l, pages[i].begin() + r, reLink); j != sregex_iterator(); ++j)
+//             ++external[i];
+//     }
+    
+//     for (int i = 0; i < n; ++i)
+//     {
+//         int l = pages[i].find("<body>");
+//         int r = pages[i].find("</body>");
+//         for (auto j = sregex_iterator(pages[i].begin() + l, pages[i].begin() + r, reLink); j != sregex_iterator(); ++j)
+//             if (um.count(j->str(1)))
+//                 link[um[j->str(1)]] += (double)basic[i] / external[i];
+//     }
+    
+//     double maxVal = 0.0;
+//     for (int i = 0; i < n; ++i)
+//     {
+//         matching[i] = basic[i] + link[i];
+//         if (maxVal < matching[i])
+//         {
+//             maxVal = matching[i];
+//             answer = i;
+//         }
+//     }
+    
+//     return answer;
+// }
